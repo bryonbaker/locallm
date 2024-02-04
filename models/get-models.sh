243575846b5bd41e05b2134542b004c2b4dcdc0f
Bryon Baker <bryonbakeraus@gmail.com>
2024-02-05 09:59:19 +1100

    Added getter and fixed launcher.

---
+++ models/get-models.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+wget https://huggingface.co/TheBloke/Llama-2-7B-Chat-GGUF/resolve/main/llama-2-7b-chat.Q5_K_S.gguf
+wget https://huggingface.co/TheBloke/Mistral-7B-Instruct-v0.1-GGUF/resolve/main/mistral-7b-instruct-v0.1.Q5_K_S.gguf
--- models/start-chatbot.sh
+++ models/start-chatbot.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
+export MODEL_FILE=llama-2-7b-chat.Q5_K_S.gguf
 podman run --name chatbot --rm -d  -it -v $(pwd)/$MODEL_FILE:/locallm/models/$MODEL_FILE:Z --env MODEL_PATH=/locallm/models/$MODEL_FILE -p 7860:7860 chatbot:service 
 
 echo "Open a web page at http://localhost:7860 to interact with the chatbot."
